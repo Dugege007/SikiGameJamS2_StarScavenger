9a6648b6281d18a74d3dacbab1fc123c6fe76fef
Dugege007 <351080175@qq.com>
2024-02-01 17:19:17 +0800

    角色控制

---
+++ Assets/Scripts/Game/PlayerSpaceShip.Designer.cs
@@ -0,0 +1,12 @@
+// Generate Id:5f486876-8a2d-41dc-bcb6-bd097b3fa1ef
+using UnityEngine;
+
+namespace StarScavenger
+{
+	public partial class PlayerSpaceShip
+	{
+
+		public UnityEngine.Rigidbody2D SelfRigidbody2D;
+
+	}
+}
